cc0479e85e0f636d87c77ea97690a1d24548bf24
Bao Nguyen <pbnguyen@ualberta.ca>
2024-08-31 00:06:50 -0600

    chore: Update next.config.mjs with environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-// next.config.mjs
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,13 +9,19 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cdn.baoopn.com',
-        pathname: '/data/img/**',
+        hostname: process.env.IMAGE_HOSTNAME,
+        pathname: process.env.IMAGE_PATH,
       },
     ],
     loader: 'imgix', // or 'cloudinary', 'akamai', etc.
     path: '',
     unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+    NEXT_PUBLIC_CURRENTLY_PLAYING_ENDPOINT: process.env.CURRENTLY_PLAYING_ENDPOINT,
+    NEXT_PUBLIC_RECENTLY_PLAYED_ENDPOINT: process.env.RECENTLY_PLAYED_ENDPOINT,
+    NEXT_PUBLIC_LEAVE_MESSAGE_ENDPOINT: process.env.LEAVE_MESSAGE_ENDPOINT,
   },
 };
 

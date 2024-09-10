882cce3fb6e1f4c98d7a938fc1588b4e8dc70c6d
Bao Nguyen <pbnguyen@ualberta.ca>
2024-09-10 01:00:38 -0600

    Refactor SpotifyRecentTracks component to handle loading and error states

--- src/components/Spotify/SpotifyNowPlaying.tsx
+++ src/components/Spotify/SpotifyNowPlaying.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState, useRef } from "react";
 import { CURRENTLY_PLAYING_ENDPOINT } from "@/app/constants";
-import { Box, Spinner, Stack, Text, Link, Progress, Image, Tooltip } from "@chakra-ui/react";
+import { Box, Spinner, Stack, Text, Link, Progress, Image, Tooltip, Flex } from "@chakra-ui/react";
 import SpotifyLogo from "@/components/Spotify/SpotifyLogo";
 import PlayingAnimation from "@/components/Spotify/PlayingAnimation";
 
 const SpotifyNowPlaying = () => {
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false); // New state variable for error tracking
   const [track, setTrack] = useState({
     albumImageUrl: '',
     artist: '',
@@ -22,16 +23,24 @@ const SpotifyNowPlaying = () => {
     const fetchCurrentlyPlaying = () => {
       if (!isMounted.current) return;
       fetch(CURRENTLY_PLAYING_ENDPOINT)
-        .then(response => response.json())
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok'); // Handle non-200 responses
+          }
+          return response.json();
+        })
         .then(track => {
           if (!isMounted.current) return;
           setTrack(track);
           setLoading(false);
+          setError(false); // Reset error state on successful fetch
           setTimeout(fetchCurrentlyPlaying, 1000);
         })
         .catch(error => {
           console.error('Error fetching currently playing track:', error);
           if (!isMounted.current) return;
+          setLoading(false); // Stop loading on error
+          setError(true); // Set error state to true
           setTimeout(fetchCurrentlyPlaying, 2000);
         });
     };
@@ -45,94 +54,105 @@ const SpotifyNowPlaying = () => {
 
   return (
     <Box width="xs">
-      {loading ?
-        <Stack align="center" mb={8}>
-          <Spinner size="md" speed="0.6s" thickness="3" color="gray.500" />
-        </Stack>
-        :
-        <Stack width="full" mb={track.isPlaying ? 2 : 4} spacing={3}>
-          <Stack spacing={2} direction="row" align="center">
-            <SpotifyLogo />
-            <Text fontWeight="semibold">{track.isPlaying ? 'Now Playing' : 'Currently Offline'}</Text>
-            {track.isPlaying && <PlayingAnimation />}
-          </Stack>
-          {!track.isPlaying &&
-            <Box p={4} borderRadius="lg" borderWidth={1}>
-              <Stack direction="column" spacing={1} align="center" textAlign="center">
-                <Image
-                  src={`https://cdn.baoopn.com/data/img/Baoo-404.png`}
-                  alt={`Bao's Image`}
-                  width={72}
-                  height={72}
-                  borderRadius="full"
-                  backgroundColor="var(--primary-pink)"
-                  border={`6px solid var(--primary-pink)`}
-                />
-                <Text
-                  fontWeight="semibold"
-                  fontSize="lg"
-                  width="full"
-                  color="alph"
-                  mt={4}
-                >
-                  Bao isn&apos;t listening right now.
-                </Text>
+      {loading ? ( // Show spinner while loading
+        <Flex direction="column" alignItems="center" justifyContent="center" height="100%">
+          <Spinner size="md" />
+          <Text mt={2}>Loading Recent Tracks...</Text>
+        </Flex>
+      ) : (
+        <>
+          {error && ( // Render error message if there's an error
+            <Text color="red.500" textAlign="center" mb={4}>
+              Failed to load currently playing track. Please try again later.
+            </Text>
+          )}
+          {/* Render the Box only if loading is false and no error occurred */}
+          {!error && (
+            <Stack width="full" mb={track.isPlaying ? 2 : 4} spacing={3}>
+              <Stack spacing={2} direction="row" align="center">
+                <SpotifyLogo />
+                <Text fontWeight="semibold">{track.isPlaying ? 'Now Playing' : 'Currently Offline'}</Text>
+                {track.isPlaying && <PlayingAnimation />}
               </Stack>
-            </Box>
-          }
-          {track.isPlaying &&
-            <Box p={4} borderRadius="lg" borderWidth={1}>
-              <Stack direction="column" spacing={4} align="center">
-                <Box position="relative">
-                  <Image
-                    src={track.albumImageUrl}
-                    alt={`${track.title} by ${track.artist}`}
-                    width={64}
-                    height={64}
-                    borderRadius="50%"
-                    className="rotating-disk"
-                  />
-                  <div className="center-circle"></div>
-                  <div className="smaller-white-circle"></div>
-                </Box>
-                <Stack spacing={1} overflow={"hidden"} width="full">
-                  <Tooltip label={track.title} alignSelf="self-start" hasArrow>
-                    <Link href={track.songUrl} alignSelf="self-start" isExternal>
-                      <Text
-                        fontWeight="semibold"
-                        fontSize="x-large"
-                        width="full"
-                        isTruncated
-                        color="alph"
-                      >
-                        {track.title}
-                      </Text>
-                    </Link>
-                  </Tooltip>
-                  <Tooltip label={track.artist} alignSelf="self-start" hasArrow>
+              {!track.isPlaying &&
+                <Box p={4} borderRadius="lg" borderWidth={1}>
+                  <Stack direction="column" spacing={1} align="center" textAlign="center">
+                    <Image
+                      src={`https://cdn.baoopn.com/data/img/Baoo-404.png`}
+                      alt={`Bao's Image`}
+                      width={72}
+                      height={72}
+                      borderRadius="full"
+                      backgroundColor="var(--primary-pink)"
+                      border={`8px solid var(--light-background)`}
+                    />
                     <Text
-                      color="gray.500"
-                      isTruncated
-                      alignSelf="self-start"
-                      mt={-3}
+                      fontWeight="semibold"
+                      fontSize="lg"
+                      width="full"
+                      color="alph"
+                      mt={4}
                     >
-                      {track.artist}
+                      Bao isn&apos;t listening right now.
                     </Text>
-                  </Tooltip>
-                  <Progress
-                    size="xs"
-                    colorScheme="green"
-                    borderRadius="md"
-                    value={(track.progress_ms / track.duration_ms) * 100}
-                  />
-                </Stack>
-              </Stack>
-            </Box>
-          }
-        </Stack>
-      }
+                  </Stack>
+                </Box>
+              }
+              {track.isPlaying &&
+                <Box p={4} borderRadius="lg" borderWidth={1}>
+                  <Stack direction="column" spacing={4} align="center">
+                    <Box position="relative">
+                      <Image
+                        src={track.albumImageUrl}
+                        alt={`${track.title} by ${track.artist}`}
+                        width={64}
+                        height={64}
+                        borderRadius="50%"
+                        className="rotating-disk"
+                      />
+                      <div className="center-circle"></div>
+                      <div className="smaller-white-circle"></div>
+                    </Box>
+                    <Stack spacing={1} overflow={"hidden"} width="full">
+                      <Tooltip label={track.title} alignSelf="self-start" hasArrow>
+                        <Link href={track.songUrl} alignSelf="self-start" isExternal>
+                          <Text
+                            fontWeight="semibold"
+                            fontSize="x-large"
+                            width="full"
+                            isTruncated
+                            color="alph"
+                          >
+                            {track.title}
+                          </Text>
+                        </Link>
+                      </Tooltip>
+                      <Tooltip label={track.artist} alignSelf="self-start" hasArrow>
+                        <Text
+                          color="gray.500"
+                          isTruncated
+                          alignSelf="self-start"
+                          mt={-3}
+                        >
+                          {track.artist}
+                        </Text>
+                      </Tooltip>
+                      <Progress
+                        size="xs"
+                        colorScheme="green"
+                        borderRadius="md"
+                        value={(track.progress_ms / track.duration_ms) * 100}
+                      />
+                    </Stack>
+                  </Stack>
+                </Box>
+              }
+            </Stack>
+          )}
+        </>
+      )}
     </Box>
-  )
+  );
 }
 
 export default SpotifyNowPlaying;
--- src/components/Spotify/SpotifyRecentTracks.tsx
+++ src/components/Spotify/SpotifyRecentTracks.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { RECENTLY_PLAYED_ENDPOINT } from "@/app/constants";
-import { Box, Link, Spinner, Stack, Text, Image, Tooltip } from "@chakra-ui/react";
+import { Box, Link, Spinner, Stack, Text, Image, Tooltip, Flex } from "@chakra-ui/react";
 import SpotifyLogo from "@/components/Spotify/SpotifyLogo";
 
 // Define the type for the track data
@@ -14,6 +14,7 @@ interface Track {
 
 const SpotifyRecentTracks = () => {
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false); // New state variable for error tracking
   const [tracks, setTracks] = useState<Track[]>([]);
   const isMounted = useRef(true);
 
@@ -21,16 +22,24 @@ const SpotifyRecentTracks = () => {
     const fetchRecentTracks = () => {
       if (!isMounted.current) return;
       fetch(RECENTLY_PLAYED_ENDPOINT)
-        .then(response => response.json())
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok'); // Handle non-200 responses
+          }
+          return response.json();
+        })
         .then((results: Track[]) => {
           if (!isMounted.current) return;
           setTracks(results);
           setLoading(false);
+          setError(false); // Reset error state on successful fetch
           setTimeout(fetchRecentTracks, 10000);
         })
         .catch(error => {
           console.error('Error fetching recently played tracks:', error);
           if (!isMounted.current) return;
+          setLoading(false); // Stop loading on error
+          setError(true); // Set error state to true
           setTimeout(fetchRecentTracks, 2000);
         });
     };
@@ -44,54 +53,65 @@ const SpotifyRecentTracks = () => {
 
   return (
     <Box width="xs">
-      {loading ? (
-        <Stack align="center" mb={8}>
-          <Spinner size="md" speed="0.6s" thickness="3" color="gray.500" />
-        </Stack>
+      {loading ? ( // Show spinner while loading
+        <Flex direction="column" alignItems="center" justifyContent="center" height="100%">
+          <Spinner size="md" />
+          <Text mt={2}>Loading Recent Tracks...</Text>
+        </Flex>
       ) : (
-        <Stack width="full" spacing={3}>
-          <Stack spacing={2} direction="row" align="center">
-            <SpotifyLogo />
-            <Text fontWeight="semibold">Recently Played</Text>
-          </Stack>
-          {tracks.map((track, index) => (
-            <Box key={index} py={1} px={2} borderRadius="lg" borderWidth={1}>
-              <Stack direction="row" spacing={3} align="center">
-                <Image
-                  alt={`${track.title} album art`}
-                  src={track.albumImageUrl}
-                  width={12}
-                  height={12}
-                  borderRadius="md"
-                />
-                <Stack spacing={0} overflow="hidden">
-                  <Tooltip label={track.title} hasArrow>
-                    <Link href={track.songUrl} alignSelf="self-start" isExternal>
-                      <Text
-                        fontWeight="semibold"
-                        width="full"
-                        isTruncated
-                        color="alph"
-                      >
-                        {track.title}
-                      </Text>
-                    </Link>
-                  </Tooltip>
-                  <Tooltip label={track.artist} hasArrow>
-                    <Text
-                      color="gray.500"
-                      isTruncated
-                      alignSelf="self-start"
-                    >
-                      {track.artist}
-                    </Text>
-                  </Tooltip>
-                  <Text></Text>
-                </Stack>
+        <>
+          {error && ( // Render error message if there's an error
+            <Text color="red.500" textAlign="center" mb={4}>
+              Failed to load recently played tracks. Please try again later.
+            </Text>
+          )}
+          {/* Render the Box only if loading is false and no error occurred */}
+          {!error && (
+            <Stack width="full" spacing={3}>
+              <Stack spacing={2} direction="row" align="center">
+                <SpotifyLogo />
+                <Text fontWeight="semibold">Recently Played</Text>
               </Stack>
-            </Box>
-          ))}
-        </Stack>
+              {tracks.map((track, index) => (
+                <Box key={index} py={1} px={2} borderRadius="lg" borderWidth={1}>
+                  <Stack direction="row" spacing={3} align="center">
+                    <Image
+                      alt={`${track.title} album art`}
+                      src={track.albumImageUrl}
+                      width={12}
+                      height={12}
+                      borderRadius="md"
+                    />
+                    <Stack spacing={0} overflow="hidden">
+                      <Tooltip label={track.title} hasArrow>
+                        <Link href={track.songUrl} alignSelf="self-start" isExternal>
+                          <Text
+                            fontWeight="semibold"
+                            width="full"
+                            isTruncated
+                            color="alph"
+                          >
+                            {track.title}
+                          </Text>
+                        </Link>
+                      </Tooltip>
+                      <Tooltip label={track.artist} hasArrow>
+                        <Text
+                          color="gray.500"
+                          isTruncated
+                          alignSelf="self-start"
+                        >
+                          {track.artist}
+                        </Text>
+                      </Tooltip>
+                      <Text></Text>
+                    </Stack>
+                  </Stack>
+                </Box>
+              ))}
+            </Stack>
+          )}
+        </>
       )}
     </Box>
   );
